98ff88e4536e24d42ddad67bc24d29ec24de98f0
fereno <fereno1989@gmail.com>
2023-05-22 21:51:58 +0330

    localStorage & useEffect

--- src/components/Login/AppPro.js
+++ src/components/Login/AppPro.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import Login from "./Login/Login";
 import Home from "./Home/Home";
@@ -7,13 +7,22 @@ import MainHeader from "./MainHeader/MainHeader";
 function AppPro() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const userLogInValue = localStorage.getItem("isLoggedIn");
+    if (userLogInValue === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
